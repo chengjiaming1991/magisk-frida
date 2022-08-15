c0956a433fd768279d09cc7d95aa601299de4ad5
chengjiaming1991 <33223591+chengjiaming1991@users.noreply.github.com>
2022-08-15 21:57:31 +0800

    不要最新版frida，使用固定的14.2.13版本

--- main.py
+++ main.py
@@ -20,25 +20,26 @@ import util
 
 
 def main():
-    last_frida_tag = util.get_last_frida_tag()
-    last_project_tag = util.get_last_project_tag()
-    last_commit_tag = util.get_last_commit_tag()
-    new_project_tag = "0"
-
-    if last_frida_tag != util.strip_revision(last_project_tag) \
-        or (last_frida_tag != util.strip_revision(last_commit_tag)
-            and util.get_commit_message().lower() == "release"):
-
-        new_project_tag = util.get_next_revision(last_frida_tag)
-        print(f"Update needed to {new_project_tag}")
-
-        # for use by deployment
-        with open("NEW_TAG.txt", "w") as the_file:
-            the_file.write(new_project_tag)
-    else:
-        print("All good!")
-
-    build.do_build(last_frida_tag, new_project_tag)
+    # last_frida_tag = util.get_last_frida_tag()
+    # last_project_tag = util.get_last_project_tag()
+    # last_commit_tag = util.get_last_commit_tag()
+    # new_project_tag = "0"
+    #
+    # if last_frida_tag != util.strip_revision(last_project_tag) \
+    #     or (last_frida_tag != util.strip_revision(last_commit_tag)
+    #         and util.get_commit_message().lower() == "release"):
+    #
+    #     new_project_tag = util.get_next_revision(last_frida_tag)
+    #     print(f"Update needed to {new_project_tag}")
+    #
+    #     # for use by deployment
+    #     with open("NEW_TAG.txt", "w") as the_file:
+    #         the_file.write(new_project_tag)
+    # else:
+    #     print("All good!")
+    # print(last_frida_tag,new_project_tag)
+    # build.do_build(last_frida_tag, new_project_tag)
+    build.do_build("14.2.13", "14.2.13-1")
 
 
 if __name__ == "__main__":
